6cda312db4997ded6a6120426132a3d88a751985
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-09-22 02:51:13 +0600

    relocated keyframes

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,49 +1,9 @@
 import React from "react";
-import { styled, keyframes } from "@stitches/local";
+import { styled } from "@stitches/local";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { violet } from "@radix-ui/colors";
 import Logo from "./Logo";
 import Link from "next/link";
-
-const enterFromRight = keyframes({
-  from: { transform: "translateX(200px)", opacity: 0 },
-  to: { transform: "translateX(0)", opacity: 1 },
-});
-
-const enterFromLeft = keyframes({
-  from: { transform: "translateX(-200px)", opacity: 0 },
-  to: { transform: "translateX(0)", opacity: 1 },
-});
-
-const exitToRight = keyframes({
-  from: { transform: "translateX(0)", opacity: 1 },
-  to: { transform: "translateX(200px)", opacity: 0 },
-});
-
-const exitToLeft = keyframes({
-  from: { transform: "translateX(0)", opacity: 1 },
-  to: { transform: "translateX(-200px)", opacity: 0 },
-});
-
-const scaleIn = keyframes({
-  from: { transform: "rotateX(-30deg) scale(0.9)", opacity: 0 },
-  to: { transform: "rotateX(0deg) scale(1)", opacity: 1 },
-});
-
-const scaleOut = keyframes({
-  from: { transform: "rotateX(0deg) scale(1)", opacity: 1 },
-  to: { transform: "rotateX(-10deg) scale(0.95)", opacity: 0 },
-});
-
-const fadeIn = keyframes({
-  from: { opacity: 0 },
-  to: { opacity: 1 },
-});
-
-const fadeOut = keyframes({
-  from: { opacity: 1 },
-  to: { opacity: 0 },
-});
 
 const NavigationMenu = styled(NavigationMenuPrimitive.Root, {
   position: "relative",
